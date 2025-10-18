5b5851602b4bf315c2661fc4d109bd6dc77aa2f7
LMB7GQ <lmb7gq@umsystem.edu>
2025-10-17 23:21:58 -0500

    FINISHED CHAPTER 30

--- M5 Space Shmup/Assets/__Scripts/Hero.cs
+++ M5 Space Shmup/Assets/__Scripts/Hero.cs
@@ -12,9 +12,14 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Set Dynamically")]
-    public float shieldLevel = 1;
+    [SerializeField]
+    private float _shieldLevel = 1;
+    private GameObject lastTriggerGo = null;
 
     void Awake()
     {
@@ -43,5 +48,58 @@ public class Hero : MonoBehaviour
 
         // Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+        if ( Input.GetKeyDown( KeyCode.Space))
+        {
+            TempFire();
+        }
     }
+
+    void TempFire ()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.linearVelocity = Vector3.up * projectileSpeed;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+
+        // print("Triggered: " + go.name); // Debug print if needed
+
+        // Make sure it's not the same triggering GameObject as last time
+        if (go == lastTriggerGo)
+        {
+            return;
+        }
+
+        lastTriggerGo = go;
+
+        if (go.tag == "Enemy")
+        {
+            // If the shield was triggered by an enemy
+            shieldLevel--;           // Decrease the level of the shield by 1
+            Destroy(go);             // Destroy the enemy
+        }
+        else
+        {
+            print("Triggered by non-Enemy: " + go.name);
+        }
+    }
+    public float shieldLevel
+    {
+        get { return _shieldLevel; }
+        set { _shieldLevel = Mathf.Min(value, 4);
+        if ( value < 0)
+            {
+                Destroy(this.gameObject);
+                Main.S.DelayedRestart( gameRestartDelay );
+            }
+        }
+    }
+
 }
